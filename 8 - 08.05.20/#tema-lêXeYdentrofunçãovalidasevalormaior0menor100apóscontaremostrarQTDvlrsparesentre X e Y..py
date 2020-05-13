a71b08805af1dac6ad37d5e49e1efe9d2122b554
Edgar Neto <edgarneto784@gmail.com>
2020-05-12 21:41:33 -0300

    atualização

--- 8 - 08.05.20/#tema-lêXeYdentrofunçãovalidasevalormaior0menor100apóscontaremostrarQTDvlrsparesentre X e Y..py
+++ 8 - 08.05.20/#tema-lêXeYdentrofunçãovalidasevalormaior0menor100apóscontaremostrarQTDvlrsparesentre X e Y..py
@@ -1,19 +1,14 @@
 #tema-lêXeYdentrofunçãovalidasevalormaior0menor100apóscontaremostrarQTDvlrsparesentre X e Y.
-def validax(a):
+def valida(a):
     while(a<0 or a>100):
-        a=int(input('Digite o Valor de X Novamente: '))
+        a=int(input('Digite o Valor Novamente: '))
     return(a)
-def validay(b):
-    while(b<0 or b>100):
-        b=int(input('Digite o Valor de Y Novamente: '))
-    return(b)
 contpar=0;
 x=int(input('Digite o Valor de X: '))
 y=int(input('Digite o Valor de Y: '))
-valx=validax(x)
-valy=validay(y)
+valx=valida(x)
+valy=valida(y)
 for i in range (valx,valy):
     if(i%2==0):
         contpar+=1
 print(f'A Quantidade de Numeros Pares Entre {valx} e {valy} é {contpar}')
-    
